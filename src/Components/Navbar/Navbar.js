9f25352c4dacce193fdbc1c58834804f90aa85fb
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-04-04 10:03:54 -0500

    added stuff

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavbarBrand } from 'react-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
+Import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-componenets';
+
+const Styles = styled.div
+    .Navbar {
+        background-color: #2222;
+    }
+
+    .Navbar-brand, .navbar-nav .nav-link {
+        color #bbb;
+    
+    &:hover {
+        color: withRouter;
+    }
+};
+
+
+
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
